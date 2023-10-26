bbf97067b9b834c08de5300495fe4a9fdfcfe462
Ashley Miles <agmiles@nic.edu>
2023-10-25 21:54:06 -0700

    Revision to Loop to Display on Page

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-var rhymePart = "little ducks went out one day, over the hill and far away. Mama duck said 'quack, quack, quack', but only ";
-let duckCount = 5;
+// const list = document.querySelector('#list');
 
-for (let i = duckCount; i >= 0; i--) {
-    if (i > 1) {
-        console.log(i + " " + rhymePart + (i - 1) + " little ducks came back.");
-    } else if (i === 1) {
-        console.log(i + " little duck went out one day, over the hill and far away. mama duck said, 'quack, quack, quack,' but no little ducks came back.");
+// var rhymePart = "little ducks went out one day, over the hill and far away. Mama duck said 'quack, quack, quack', but only ";
+// let duckCount = 5;
+
+// for (let i = duckCount; i >= 0; i--) {
+//     if (i > 1) {
+//         console.log(i + " " + rhymePart + (i - 1) + " little ducks came back.");
+//     } else if (i === 1) {
+//         console.log(i + " little duck went out one day, over the hill and far away. mama duck said, 'quack, quack, quack,' but no little ducks came back.");
+//     } else {
+//         console.log("No little ducks came back.");
+//     }
+
+//     const verse = document.createElement('p');
+//     verse.textContent = 
+// }
+
+
+const list = document.querySelector('#list');
+
+var rhymePart = "little ducks went out one day, over the hill and far away. Mama duck said 'quack, quack, quack', but only ";
+
+for (var duckCount = 5; duckCount >= 0; duckCount--) {
+    let textString = " ";
+
+    if (duckCount > 1) {
+        textString = duckCount + " " + rhymePart + (duckCount - 1) + " little ducks came back."
+    } else if (duckCount === 1) {
+        textString = duckCount + " little duck went out one day, over the hill and far away. Mama duck said 'quack, quack, quack', but no little ducks came back.";
     } else {
-        console.log("No little ducks came back.");
+        textString = "No little ducks came back.";
     }
+
+    const verse = document.createElement('p');
+    verse.textContent = textString;
+
+    list.appendChild(verse);
 }
